1fefe095ad1a364b43c75a3dcd1d5bfc78187cc6
kefan1242 <kefan4601@gmail.com>
2025-04-16 21:30:29 +0800

    商品浏览用户端业务

---
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package com.sky.mapper;
+
+import com.sky.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface UserMapper {
+    @Select("Select *from user where openid=#{openid}")
+    User getByOpenId(@Param("openid") String openid);
+
+    void insert(User user);
+}
